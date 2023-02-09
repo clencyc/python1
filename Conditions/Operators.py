07f21bd98e838f64122d28aa923d4c5e121e7492
emobilis <clency2023@gmail.com>
2023-02-09 09:40:48 +0300

    one commit

--- Conditions/Operators.py
+++ Conditions/Operators.py
@@ -1,14 +1,3 @@
 
-
-
-dial = int(input("Enter a number:"))
-if dial == 1:
-    print("Enrol for Bonga points")
-if dial == 2:
-    print("Okoa Jahazi",
-          "Data deals")
-if dial == 3:
-    print("Data with no expiry")
-elif dial ==4:
-    print("There is no option chosen")
-
+print(15 > 24)
+print(45 == 45)
