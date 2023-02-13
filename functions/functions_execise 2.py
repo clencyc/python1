75a651ecd7e274d3717b274bb11708b52f605673
emobilis <clency2023@gmail.com>
2023-02-13 09:03:53 +0300

    multiplying functions

---
+++ functions/functions_execise 2.py
@@ -0,0 +1,11 @@
+#write a function that will take 2 numbers
+#multiply them and print the total
+
+def calc_num1():
+    num1 = int(input("Enter a number:"))
+    num2 = int(input("Enter another number:"))
+    total = num1 * num2
+    print(f"The product is {total}.")
+
+
+calc_num1()
